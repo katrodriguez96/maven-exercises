09b8b3375dd25a849713de4df65c8152a1b91e32
Kathryn Rodriguez <kathryn.n.rodriguez@gmail.com>
2022-03-08 15:37:05 -0600

    completed dependency management exercise

--- src/main/java/ExampleClass.java
+++ src/main/java/ExampleClass.java
@@ -1,7 +1,15 @@
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.Scanner;
+
 public class ExampleClass {
     public static void main(String[] args) {
-        System.out.println("StringUtils.reverse(\"Kat\") = " + StringUtils.reverse("Kat"));
+//        System.out.println("StringUtils.reverse(\"Kat\") = " + StringUtils.reverse("Kat"));
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter something: ");
+        String userInput = sc.nextLine();
+        System.out.println("StringUtils.isNumeric(userInput) = " + StringUtils.isNumeric(userInput));
+        System.out.println("StringUtils.swapCase(userInput) = " + StringUtils.swapCase(userInput));
+        System.out.println("StringUtils.reverse(userInput) = " + StringUtils.reverse(userInput));
     }
 }
